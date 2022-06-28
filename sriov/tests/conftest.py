462fb5151eaf9911ef60139a5f12f8fdf6280c51
Daniel Kostecki <dkosteck@redhat.com>
2022-06-27 20:11:19 -0400

    Implementation of SR-IOV.MultipleVFCreation.withMTU:
    - Allow longer timeouts (with default 5 seconds) in execute
    - Implement the test in the SR-IOV.MultipleVFCreation.withMTU directory
    - Add reused fixtures to conftest

--- sriov/tests/conftest.py
+++ sriov/tests/conftest.py
@@ -16,3 +16,27 @@ def dut(settings):
     user = settings.config["dut"]["username"]
     password = settings.config["dut"]["password"]
     return ShellHandler(host, user, password)
+
+@pytest.fixture(autouse=True)
+def initialization(dut, settings, pf_net_path):
+    reset_command(dut, settings, pf_net_path)
+
+@pytest.fixture()
+def pf_name(settings):
+    pf_name = settings.config["dut"]["interface"]["pf1"]["name"]
+    return pf_name
+
+@pytest.fixture()
+def pf_net_path(settings, pf_name):
+    pf_net_path = "/sys/class/net/"+ pf_name + '/device'
+    return pf_net_path
+
+@pytest.fixture
+def reset_vfs(dut, settings, pf_net_path):
+    yield
+    reset_command(dut, settings, pf_net_path)
+
+def reset_command(dut, settings, pf_net_path):
+    clear_vfs = "echo 0 > " + pf_net_path + "/sriov_numvfs"
+    code, out, err = dut.execute(clear_vfs, 60)
+    assert code == 0
