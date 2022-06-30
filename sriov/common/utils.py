c1deef11d1834e8f6a19f767f64c3293f322a424
Jianzhu Zhang <jianzzha@redhat.com>
2022-06-29 22:34:20 -0400

    update utils.py

--- sriov/common/utils.py
+++ sriov/common/utils.py
@@ -1,5 +1,3 @@
-from sriov.common.exec import ShellHandler
-
 def get_pci_address(ssh_obj, iface):
     """
     :param ssh_obj: ssh_obj to the remote host
@@ -32,8 +30,7 @@ def bind_driver(ssh_obj, pci, driver):
     return True    
 
 def config_interface(ssh_obj, intf, vlan, ip):
-    """    config an ip address on vlan interface; if vlan is 0, config ip on main interface
-
+    """config an ip address on vlan interface; if vlan is 0, config ip on main interface
 
     Args:
         ssh_obj: ssh connection obj
@@ -57,8 +54,8 @@ def config_interface(ssh_obj, intf, vlan, ip):
         if code != 0:
             raise Exception(err)
         
-def clear_interface(ssh_obj, intf, vlan):
-    """    clear the ip address from the vlan interface and the main interface
+def clear_interface(ssh_obj, intf, vlan=0):
+    """clear the ip address from the vlan interface and the main interface
 
     Args:
         ssh_obj: ssh connection obj
@@ -68,11 +65,14 @@ def clear_interface(ssh_obj, intf, vlan):
     Raises:
         Exception: stderr msg in array
     """
-    steps = [
-            f"ip addr flush dev {intf}.{vlan} || true",
-            f"ip link del {intf}.{vlan} || true",
-            f"ip addr flush dev {intf} || true"
-            ]
+    if vlan != 0:
+        steps = [
+                f"ip addr flush dev {intf}.{vlan} || true",
+                f"ip link del {intf}.{vlan} || true",
+                f"ip addr flush dev {intf} || true"
+                ]
+    else:
+        steps = [f"ip addr flush dev {intf} || true"]
     for step in steps:
         code, _, err = ssh_obj.execute(step)
         if code != 0:
@@ -108,3 +108,46 @@ def rm_arp_entry(ssh_obj, ip):
     code, _, err = ssh_obj.execute(cmd)
     if code != 0:
         raise Exception(err)
+    
+def start_tmux(ssh_obj, name, cmd):
+    """Run cmd in a tmux session
+
+    Args:
+        ssh_obj: ssh connection obj
+        name: tmux session name
+        cmd (str): a single command to run
+    """
+    
+    steps = [
+            f"tmux kill-session -t {name} || true",
+            f"tmux new-session -s {name} -d {cmd}"
+            ]
+    
+    for step in steps:
+        code, _, err = ssh_obj.execute(step)
+        if code != 0:
+            raise Exception(err)
+
+def stop_tmux(ssh_obj, name):
+    """stop tmux session
+
+    Args:
+        ssh_obj: ssh connection obj
+        name (str): tmux session name
+    """
+    code, _, err = ssh_obj.execute(f"tmux kill-session -t {name} || true")
+    if code != 0:
+        raise Exception(err)
+
+def get_intf_mac(ssh_obj, intf):
+    """get mac address from the interface name
+
+    Args:
+        ssh_obj (_type_): ssh connection obj
+        intf (str): interface name
+    """
+    cmd = f"cat /sys/class/net/{intf}/address"
+    code, out, err = ssh_obj.execute(cmd)
+    if code != 0:
+        raise Exception(err)
+    return out[0].strip("\n")
