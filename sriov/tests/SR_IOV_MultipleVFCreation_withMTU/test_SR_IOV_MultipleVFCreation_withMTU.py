2ffa67daa99c43a2f0cbeb52d35372973ebd0887
Daniel Kostecki <dkosteck@redhat.com>
2022-07-06 15:22:01 -0400

    Inline comment for execution_number

--- sriov/tests/SR_IOV_MultipleVFCreation_withMTU/test_SR_IOV_MultipleVFCreation_withMTU.py
+++ sriov/tests/SR_IOV_MultipleVFCreation_withMTU/test_SR_IOV_MultipleVFCreation_withMTU.py
@@ -9,6 +9,7 @@ from pytest_html import extras
 
 LOGGER = logging.getLogger(__name__)
 
+# Adjust the execution_number parameter for desired amount of repeated tests (or to 100 for actual testing)
 @pytest.mark.parametrize('execution_number', range(1))
 def test_SRIOVMultipleVFCreationwithMTU(dut, settings, testdata, execution_number):
     for pf in testdata['pfs']:
