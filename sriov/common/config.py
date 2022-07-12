9cabde7bf6ada5d8ed07fdab017af993a7271a3b
Daniel Kostecki <dkosteck@redhat.com>
2022-07-12 16:36:13 -0400

    Update config documentation comments

--- sriov/common/config.py
+++ sriov/common/config.py
@@ -2,5 +2,11 @@ import yaml
 
 class Config:
     def __init__(self, config_file):
+        """ Init the config file object
+
+        Args:
+            self:              ssh_obj to the remote host
+            config_file (str): path to the config file
+        """
         with open(config_file, 'r') as file:
             self.config = yaml.safe_load(file)
