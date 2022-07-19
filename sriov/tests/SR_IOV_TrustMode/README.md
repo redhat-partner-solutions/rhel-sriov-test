b9b60bdc631237aa266d1b060c7f2af40f991fe9
Andrew Kiselev <akiselev@redhat.com>
2022-07-19 14:50:29 -0400

    Ignore error when trying to change mac in trust mode off

--- sriov/tests/SR_IOV_TrustMode/README.md
+++ sriov/tests/SR_IOV_TrustMode/README.md
@@ -18,10 +18,17 @@ ip link set ${DUT_PF}v0 up
 
 * Read VF 0 mac address and assert it is equal to ${MAC_2}
 
-* On DUT, assert on 0 exit code of each of the following steps,
+* On DUT, assert on 0 exit code of the following step,
+
 ```
 ip link set ${DUT_PF} vf 0 trust off
+```
+
+* The next step is expected to produce an error,
+  
+```
 ip link set ${DUT_PF}v0 address ${MAC_3}
+RTNETLINK answers: Permission denied
 ```
 
 * Read VF 0 mac address and assert it is not equal to ${MAC_3}
--- sriov/tests/SR_IOV_TrustMode/test_SR_IOV_TrustMode.py
+++ sriov/tests/SR_IOV_TrustMode/test_SR_IOV_TrustMode.py
@@ -23,25 +23,31 @@ def test_SR_IOV_TrustMode(dut, settings):
 
     # check if vf 0 mac address is equal to mac_2
     cmd = "ip link show {}".format(pf) + " | awk '/vf 0/{print $4;}'"
+    print(cmd)
     code, out, err = dut.execute(cmd)
     assert code == 0, err
     vf_mac = out[0].strip("\n")
+    print(vf_mac)
     assert vf_mac == mac_2
     
-    # set trust mode to on
-    steps = [
-        f"ip link set {pf} vf 0 trust off",
-        f"ip link set {pf}v0 address {mac_3}"
-        ]
-    for step in steps:
-        print(step)
-        code, out, err = dut.execute(step)
-        assert code == 0, err
-        time.sleep(0.1)
-        
+    # set trust mode to off
+    cmd = "ip link set {}".format(pf) + " vf 0 trust off"
+    print(cmd)
+    code, out, err = dut.execute(cmd)
+    assert code == 0, err
+    time.sleep(0.1)
+    
+    # try to overwrite mac_2 with mac_3
+    cmd = "ip link set {}".format(pf) + "v0 address {}".format(mac_3)
+    print(cmd)
+    code, out, err = dut.execute(cmd)
+    print(err[0].strip("\n"))
+           
     # check if vf 0 mac address is NOT equal to mac_3
     cmd = "ip link show {}".format(pf) + " | awk '/vf 0/{print $4;}'"
+    print(cmd)
     code, out, err = dut.execute(cmd)
     assert code == 0, err
     vf_mac = out[0].strip("\n")
+    print(vf_mac)
     assert vf_mac != mac_3    
