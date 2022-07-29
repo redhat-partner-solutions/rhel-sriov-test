ae18c81e2abf447980c57626f8b885ad66a369ce
Daniel Kostecki <dkosteck@redhat.com>
2022-07-29 16:02:11 -0400

    Refactoring SR_IOV_macAddress using execute_and_assert and execute_until_timeout

--- sriov/tests/SR_IOV_macAddress/test_SR_IOV_macAddress.py
+++ sriov/tests/SR_IOV_macAddress/test_SR_IOV_macAddress.py
@@ -17,27 +17,24 @@ def test_SR_IOV_macAddress(dut, trafficgen, settings, testdata):
     vf0_mac = testdata['dut_mac']
     pf = settings.config["dut"]["interface"]["pf1"]["name"]
     steps = [
-        "echo 0 > /sys/class/net/{}/device/sriov_numvfs".format(pf),
-        "echo 1 > /sys/class/net/{}/device/sriov_numvfs".format(pf),
         "ip link set {}v0 down".format(pf),
         "ip link set {} vf 0 mac {}".format(pf, vf0_mac),
         "ip link set {}v0 up".format(pf),
         "ip add add {}/24 dev {}v0".format(dut_ip, pf) 
         ]
-    for step in steps:
-        print(step)
-        code, out, err = dut.execute(step)
-        assert code == 0, err
-        time.sleep(0.1)
+
+    create_vfs(dut, pf, 1)
+
+    execute_and_assert(dut, steps, 0, 0.1)
 
     trafficgen_pf = settings.config["trafficgen"]["interface"]["pf1"]["name"]
     trafficgen_vlan = 0
     clear_interface(trafficgen, trafficgen_pf, trafficgen_vlan) 
     config_interface(trafficgen, trafficgen_pf, trafficgen_vlan, trafficgen_ip)
     add_arp_entry(trafficgen, dut_ip, vf0_mac)
-    code, out, err = trafficgen.execute("ping -W 1 -c 1 {}".format(testdata['dut_ip']))
+    ping_cmd = "ping -W 1 -c 1 {}".format(testdata['dut_ip'])
+    print(ping_cmd)
+    ping_result = execute_until_timeout(trafficgen, ping_cmd)
     rm_arp_entry(trafficgen, trafficgen_ip)
     clear_interface(trafficgen, trafficgen_pf, trafficgen_vlan)
-    assert code == 0, err
-
-        
+    assert ping_result  
