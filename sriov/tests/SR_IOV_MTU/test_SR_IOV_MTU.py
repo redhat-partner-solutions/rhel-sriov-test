ff6bd0c0294a04f9307530370b710f0a54403707
Daniel Kostecki <dkosteck@redhat.com>
2022-07-29 11:57:50 -0400

    Refactored SR_IOV_MTU using execute_and_assert

--- sriov/tests/SR_IOV_MTU/test_SR_IOV_MTU.py
+++ sriov/tests/SR_IOV_MTU/test_SR_IOV_MTU.py
@@ -16,23 +16,14 @@ def test_SR_IOV_MTU(dut, trafficgen, settings, testdata):
 
     dut_ip = testdata['dut_ip']
     pf = settings.config["dut"]["interface"]["pf1"]["name"]
-    steps = [
-        f"echo 0 > /sys/class/net/{pf}/device/sriov_numvfs",
-        f"echo 1 > /sys/class/net/{pf}/device/sriov_numvfs",
-    ]
-    for step in steps:
-        print(step)
-        code, out, err = dut.execute(step)
-        assert code == 0, err
-        time.sleep(1)
+    
+    assert create_vfs(dut, pf, 1)
 
     # command to get the maxmtu from the DUT
-    cmd = f"ip -d link list {pf}"
-    print(cmd)
-    code, out, err = dut.execute(cmd)
-    assert code == 0
+    cmd = [f"ip -d link list {pf}"]
+    outs, errs = execute_and_assert(dut, cmd, 0)   
     dut_mtu = 0
-    for line in out:
+    for line in outs[0]:
         match = re.search(r'maxmtu (\d+)', line)
         if match is not None:
             dut_mtu = int(match.group(1))
@@ -42,9 +33,9 @@ def test_SR_IOV_MTU(dut, trafficgen, settings, testdata):
     # get trafficgen maxmtu
     trafficgen_pf = settings.config["trafficgen"]["interface"]["pf1"]["name"]
     trafficgen_mtu = 0
-    code, out, err = trafficgen.execute(f"ip -d link list {trafficgen_pf}")
-    assert code == 0
-    for line in out:
+    cmd = [f"ip -d link list {trafficgen_pf}"]
+    outs, errs = execute_and_assert(trafficgen, cmd, 0)
+    for line in outs[0]:
         match = re.search(r'maxmtu (\d+)', line)
         if match is not None:
             trafficgen_mtu = int(match.group(1))
@@ -54,18 +45,17 @@ def test_SR_IOV_MTU(dut, trafficgen, settings, testdata):
     # use the smaller mtu between dut and trafficgen
     mtu = min(dut_mtu, trafficgen_mtu)
     
-    trafficgen.execute(f"ip link set {trafficgen_pf} mtu {mtu}")
+    cmd = [f"ip link set {trafficgen_pf} mtu {mtu}"]
+    execute_and_assert(trafficgen, cmd, 0)
+    
     steps = [
         f"ip link set {pf} mtu {mtu}",
         f"ip link set {pf}v0 mtu {mtu}",
         f"ip link set {pf}v0 up",
         f"ip add add {dut_ip}/24 dev {pf}v0"
     ]
-    for step in steps:
-        print(step)
-        code, out, err = dut.execute(step)
-        assert code == 0, err
-        time.sleep(0.1)
+    
+    execute_and_assert(dut, steps, 0, 0.1)
 
     vf0_mac = get_vf_mac(dut, pf, 0)
     trafficgen_ip = testdata['trafficgen_ip']
@@ -75,7 +65,9 @@ def test_SR_IOV_MTU(dut, trafficgen, settings, testdata):
     config_interface(trafficgen, trafficgen_pf, trafficgen_vlan, trafficgen_ip)
     add_arp_entry(trafficgen, dut_ip, vf0_mac)
     add_arp_entry(dut, trafficgen_ip, trafficgen_mac)
-
+    
+    # Special case: ARP entries may not be populated before first ping, 
+    # so we wait and execute the ping multiple times if needed
     ping_cmd = f"ping -W 1 -c 1 -s {mtu-28} -M do {trafficgen_ip}"
     print(ping_cmd)
     ping_result = execute_until_timeout(dut, ping_cmd)
@@ -84,8 +76,10 @@ def test_SR_IOV_MTU(dut, trafficgen, settings, testdata):
     rm_arp_entry(trafficgen, dut_ip)
     clear_interface(trafficgen, trafficgen_pf, trafficgen_vlan)
     rm_arp_entry(dut, trafficgen_ip)
-    trafficgen.execute(f"ip link set {trafficgen_pf} mtu 1500")
-    dut.execute(f"echo 0 > /sys/class/net/{pf}/device/sriov_numvfs")
-    dut.execute(f"ip link set {pf} mtu 1500")
+    cmd = [f"ip link set {trafficgen_pf} mtu 1500"]
+    execute_and_assert(trafficgen, cmd, 0)
+    cmds = [f"echo 0 > /sys/class/net/{pf}/device/sriov_numvfs",
+            f"ip link set {pf} mtu 1500"]
+    execute_and_assert(dut, cmds, 0)
     
     assert ping_result
