188cd571c37328df80be104cfd27d253426d4ccd
Andrew Kiselev <akiselev1@users.noreply.github.com>
2022-08-03 16:41:11 -0400

    Refactor InterVF and macAddress cases to pass linter (#67)
    * Refactor InterVF and macAddress cases to pass linter
    * Fix escaped long lines

--- sriov/tests/SR_IOV_macAddress/test_SR_IOV_macAddress.py
+++ sriov/tests/SR_IOV_macAddress/test_SR_IOV_macAddress.py
@@ -1,8 +1,13 @@
-import time
-from sriov.common.utils import *
+from sriov.common.utils import (
+    create_vfs,
+    execute_and_assert,
+    prepare_ping_test,
+    execute_until_timeout,
+)
+
 
 def test_SR_IOV_macAddress(dut, trafficgen, settings, testdata):
-    """ Test and ensure that VF MAC address functions as intended
+    """Test and ensure that VF MAC address functions as intended
 
     Args:
         dut:         ssh connection obj
@@ -19,8 +24,8 @@ def test_SR_IOV_macAddress(dut, trafficgen, settings, testdata):
         "ip link set {}v0 down".format(pf),
         "ip link set {} vf 0 mac {}".format(pf, vf0_mac),
         "ip link set {}v0 up".format(pf),
-        "ip add add {}/24 dev {}v0".format(dut_ip, pf) 
-        ]
+        "ip add add {}/24 dev {}v0".format(dut_ip, pf),
+    ]
 
     create_vfs(dut, pf, 1)
 
@@ -29,14 +34,18 @@ def test_SR_IOV_macAddress(dut, trafficgen, settings, testdata):
     trafficgen_pf = settings.config["trafficgen"]["interface"]["pf1"]["name"]
     trafficgen_mac = settings.config["trafficgen"]["interface"]["pf1"]["mac"]
     trafficgen_vlan = 0
-    prepare_ping_test(trafficgen, trafficgen_pf, trafficgen_vlan,
-                      trafficgen_ip, trafficgen_mac,
-                      dut, dut_ip, vf0_mac,
-                      testdata)
-    
+    prepare_ping_test(
+        trafficgen,
+        trafficgen_pf,
+        trafficgen_vlan,
+        trafficgen_ip,
+        trafficgen_mac,
+        dut,
+        dut_ip,
+        vf0_mac,
+        testdata,
+    )
+
     ping_cmd = "ping -W 1 -c 1 {}".format(testdata['dut_ip'])
     print(ping_cmd)
     assert execute_until_timeout(trafficgen, ping_cmd)
-
-
-        
--- sriov/tests/SR_IOV_macAddress_DPDK/test_SR_IOV_macAddress_DPDK.py
+++ sriov/tests/SR_IOV_macAddress_DPDK/test_SR_IOV_macAddress_DPDK.py
@@ -1,11 +1,16 @@
-import pytest
-import logging
-from sriov.common.exec import ShellHandler
-from sriov.common.utils import *
+from sriov.common.utils import (
+    create_vfs,
+    get_pci_address,
+    execute_and_assert,
+    bind_driver,
+    prepare_ping_test,
+    execute_until_timeout,
+)
 
 
 def test_SR_IOV_macAddress_DPDK(dut, trafficgen, settings, testdata):
-    """ Test and ensure that VF MAC address functions as intended when bound to DPDK driver
+    """Test and ensure that VF MAC address functions as intended when bound
+       to DPDK driver
 
     Args:
         dut:         ssh connection obj
@@ -25,37 +30,40 @@ def test_SR_IOV_macAddress_DPDK(dut, trafficgen, settings, testdata):
     cmd = ["ip link set {} vf 0 mac {}".format(pf, vf0_mac)]
     execute_and_assert(dut, cmd, 0)
 
-    vf_pci = get_pci_address(dut, pf+"v0")
+    vf_pci = get_pci_address(dut, pf + "v0")
     assert bind_driver(dut, vf_pci, "vfio-pci")
-    
+
     dpdk_img = settings.config["dpdk_img"]
     cpus = settings.config["dut"]["pmd_cpus"]
-    podman_cmd = "podman run -it --rm --privileged "\
-                 "-v /sys:/sys -v /dev:/dev -v /lib/modules:/lib/modules "\
-                 "--cpuset-cpus {} {} dpdk-testpmd -l {} -n 4 -a {} "\
-                 "-- --nb-cores=2 -i".format(cpus, dpdk_img, cpus, vf_pci)
+    podman_cmd = (
+        "podman run -it --rm --privileged "
+        "-v /sys:/sys -v /dev:/dev -v /lib/modules:/lib/modules "
+        "--cpuset-cpus {} {} dpdk-testpmd -l {} -n 4 -a {} "
+        "-- --nb-cores=2 -i".format(cpus, dpdk_img, cpus, vf_pci)
+    )
     print(podman_cmd)
     dut.start_testpmd(podman_cmd)
     assert dut.testpmd_active()
-    
-    steps = [
-        "set fwd icmpecho",
-        "start"
-        ]
+
+    steps = ["set fwd icmpecho", "start"]
     for step in steps:
         code = dut.testpmd_cmd(step)
         assert code == 0
 
     trafficgen_vlan = 0
-    trafficgen_mac = None   #None means no need to set arp entry on DUT
-    prepare_ping_test(trafficgen, trafficgen_pf, trafficgen_vlan,
-                      trafficgen_ip, trafficgen_mac,
-                      dut, dut_ip, vf0_mac,
-                      testdata)   
-    
+    trafficgen_mac = None  # None means no need to set arp entry on DUT
+    prepare_ping_test(
+        trafficgen,
+        trafficgen_pf,
+        trafficgen_vlan,
+        trafficgen_ip,
+        trafficgen_mac,
+        dut,
+        dut_ip,
+        vf0_mac,
+        testdata,
+    )
+
     ping_cmd = "ping -W 1 -c 1 {}".format(dut_ip)
     print(ping_cmd)
     assert execute_until_timeout(trafficgen, ping_cmd)
-
-
-        
