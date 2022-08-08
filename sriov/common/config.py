485a1c0dba9c6108e14788c96f561c79c4bfe33a
Jianzhu Zhang <jianzzha@redhat.com>
2022-08-08 13:53:11 -0400

    seperate testbed info from config data

--- sriov/common/config.py
+++ sriov/common/config.py
@@ -2,12 +2,17 @@ import yaml
 
 
 class Config:
-    def __init__(self, config_file: str) -> None:
+    def __init__(self, config_file: str, testbed_file: str) -> None:
         """Init the config file object
 
         Args:
             self:
             config_file (str): path to the config file
+            testbed_file (str): path to the testbed file
         """
         with open(config_file, "r") as file:
             self.config = yaml.safe_load(file)
+        with open(testbed_file, "r") as file:
+            testbed = yaml.safe_load(file)
+            for k, v in testbed.items():
+                self.config[k] = v       
