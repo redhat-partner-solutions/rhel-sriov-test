4096229a60e32b6e38df62520dad5307d0e428c5
Jianzhu Zhang <jianzzha@redhat.com>
2022-08-29 15:13:03 -0400

    add doc in utils.py

--- sriov/common/utils.py
+++ sriov/common/utils.py
@@ -654,18 +654,35 @@ def execute_until_timeout(ssh_obj: ShellHandler, cmd: str, timeout: int = 10) ->
     print("\nstdout:" + str(out) + "\nstderr:" + str(err))
     return False
 
-def wait_tmux_testpmd_ready(ssh_obj: ShellHandler, tmux_session: str, timeout: int) -> bool:
+def wait_tmux_testpmd_ready(ssh_obj: ShellHandler, tmux_session: str,
+                            timeout: int) -> bool:
+    """wait the testpmd in a tmux session ready
+
+    Args:
+        ssh_obj (ShellHandler): ssh connection obj
+        tmux_session (str): tmux session name
+        timeout (int): how many seconds to wait
+
+    Returns:
+        bool: True if success; False otherwise
+    """
     for i in range(timeout):
         time.sleep(1)
         cmd = [f"tmux capture-pane -pt {tmux_session}"]
         outs, errs = execute_and_assert(ssh_obj, cmd, 0)
-        started = False
         for line in outs[0]:
             if line.startswith("Press enter to exit"):
                 return True
     return False
 
-def stop_testpmd_in_tmux(ssh_obj: ShellHandler, tmux_session: str):
+def stop_testpmd_in_tmux(ssh_obj: ShellHandler,
+                        tmux_session: str) -> None:
+    """stop the testpmd in a tmux session
+
+    Args:
+        ssh_obj (ShellHandler): ssh connection obj
+        tmux_session (str): tmux session name
+    """
     cmd = f"tmux send-keys -t {tmux_session} 'quit' ENTER"
     ssh_obj.log_str(cmd)
     ssh_obj.execute(cmd)
