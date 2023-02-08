a3fdd63fb445bb20c3d15a94b46dba343bf46cf1
Jianzhu Zhang <jianzzha@redhat.com>
2023-02-08 11:27:21 -0500

    Add mtu and vlan settings

--- sriov/common/utils.py
+++ sriov/common/utils.py
@@ -988,3 +988,10 @@ def delete_ipv6_neighbor(ssh_obj: ShellHandler, ipv6: str):
         return
     cmd = [f"ip -6 neigh del {ipv6} dev {intf}"]
     execute_and_assert(ssh_obj, cmd, 0)
+
+
+def switch_detected(ssh_obj: ShellHandler, interface: str) -> bool:
+    cmd = f"timeout 3 tcpdump -i {interface} -c 1 stp"
+    ssh_obj.log_str(cmd)
+    code, _, _ = ssh_obj.execute(cmd)
+    return code == 0
